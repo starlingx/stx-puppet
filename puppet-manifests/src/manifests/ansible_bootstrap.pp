openstack-archive/stx-config

Ansible Bootstrap Deployment
This commit is initial submission of bootstrap playbook which
enables the bootstrap of initial controller. The playbook
defaults are meant for configuring the localhost in vbox
development environment. Custom hosts file and user overrides
are required for configuring multiple hosts and lab specific setup.
Secret file and SSH keys are required for production test enviroment.

Tests performed:
 - installation
 - config_controller complete to ensure the current method of
   configuring the first controller is intact
 - localhost bootstrap with default hosts file
 - multiple remote hosts bootstrap with custom hosts file
 - reconfigurations with user overrides
 - AIOSX unlock (a custom build of maintenance agent is required.
   A long-term solution is in the works.)
 - Failure & skip play cases (invalid config inputs, incorrect load,
   connection failure, no changes replay, etc...)

TODO:
 - Maintenance update for bootstrap playbook
 - AIODX and Standard system tests
 - Port recent changes to config_controller to Playbook
 - Docker proxy/custom registry related tests
 - Package bootstrap playbook in SDK
 - Config_controller cleanup

Story: 200476
Task: 29686
Task: 29687

Change-Id: If553f1eeed32606bacc690ef277e60606e9d93ea
Signed-off-by: Tee Ngo <tee.ngo@windriver.com>

puppet-manifests/src/manifests/ansible_bootstrap.pp
@@ -1,0 +1,32 @@
+#
+# puppet manifest for controller initial bootstrap
+#
+
+Exec {
+  timeout => 600,
+  path => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:/usr/local/sbin'
+}
+
+include ::platform::config::bootstrap
+include ::platform::users::bootstrap
+include ::platform::ldap::bootstrap
+include ::platform::drbd::bootstrap
+include ::platform::postgresql::bootstrap
+include ::platform::amqp::bootstrap
+
+include ::openstack::keystone::bootstrap
+include ::openstack::barbican::bootstrap
+include ::platform::client::bootstrap
+include ::openstack::client::bootstrap
+
+include ::platform::sysinv::bootstrap
+
+# Puppet classes to enable the bring up of kubernetes master
+include ::platform::docker::bootstrap
+include ::platform::etcd::bootstrap
+
+# Puppet classes to enable initial controller unlock
+include ::platform::drbd::dockerdistribution::bootstrap
+include ::platform::mtce::bootstrap
+include ::platform::nfv::bootstrap
+include ::platform::fm::bootstrap

starlingx/stx-puppet

 - stx-application applied in AIOSX and AIODX
 - Support for standard & storage configurations
Co-Authored-By: Ovidiu Poncea <ovidiu.poncea@windriver.com>

puppet-manifests/src/manifests/ansible_bootstrap.pp
@@ -1,0 +1,31 @@
+#
+# puppet manifest for controller initial bootstrap
+#
+
+Exec {
+  timeout => 600,
+  path => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:/usr/local/sbin'
+}
+
+include ::platform::config::bootstrap
+include ::platform::users::bootstrap
+include ::platform::ldap::bootstrap
+include ::platform::drbd::bootstrap
+include ::platform::postgresql::bootstrap
+include ::platform::amqp::bootstrap
+
+include ::openstack::keystone::bootstrap
+include ::openstack::barbican::bootstrap
+include ::platform::client::bootstrap
+
+include ::platform::sysinv::bootstrap
+
+# Puppet classes to enable the bring up of kubernetes master
+include ::platform::docker::bootstrap
+include ::platform::etcd::bootstrap
+
+# Puppet classes to enable initial controller unlock
+include ::platform::drbd::dockerdistribution::bootstrap
+include ::platform::filesystem::backup
+include ::platform::mtce::bootstrap
+include ::platform::fm::bootstrap