3722db3e545f6d8c01efa3fac3949bd2ac78cf50
Dan Voiculeasa <dan.voiculeasa@windriver.com>
2020-10-29 10:40:43 +0200

    Make sure OSDs are placed on nodes
    With the recent work that added the possibility to take a backup on
    controller-1 and restore on controller-0 the [osd.X] entries are removed
    from ceph.conf before controller-0 unlock.
    
    An [osd.X] entry must be present in ceph.conf so that a ceph-osd process
    is spawned and the directory structure for that osd is created in
    /var/lib/ceph/osd/ceph-X.
    
    Mtc runs an initialization script which expects that directory to be
    populated, otherwise it fails and places the node in a degraded state.
    
    For AIO systems the [osd] entry was present populated, but for STANDARD
    it was not. This commit ensures the entry is populated for both AIO and
    STANDARD systems.
    
    Tested full install + platform B&R on AIO-SX, AIO-DX, STANDARD with
    controller storage, all with ceph.
    
    Closes-Bug: 1901636
    Change-Id: I669ad3b22f59136253d42cdf6ac603fba31000be
    Signed-off-by: Dan Voiculeasa <dan.voiculeasa@windriver.com>

--- puppet-manifests/src/modules/platform/manifests/ceph.pp
+++ puppet-manifests/src/modules/platform/manifests/ceph.pp
@@ -333,15 +333,25 @@ define osd_crush_location(
   $journal_path,
   $tier_name,
 ) {
-  ceph_config{
-    "osd.${$osd_id}/devs": value => $data_path;
-  }
   # Only set the crush location for additional tiers
   if $tier_name != 'storage' {
     ceph_config {
       "osd.${$osd_id}/host":           value => "${$::platform::params::hostname}-${$tier_name}";
       "osd.${$osd_id}/crush_location": value => "root=${tier_name}-tier host=${$::platform::params::hostname}-${$tier_name}";
     }
+  }
+}
+
+define osd_location(
+  $osd_id,
+  $osd_uuid,
+  $disk_path,
+  $data_path,
+  $journal_path,
+  $tier_name,
+) {
+  ceph_config {
+    "osd.${$osd_id}/devs": value => $data_path;
   }
 }
 
@@ -432,6 +442,12 @@ class platform::ceph::osds(
     create_resources('platform_ceph_osd', $osd_config)
     create_resources('platform_ceph_journal', $journal_config)
   }
+
+  # Ensure ceph.conf is created
+  Class['::ceph'] -> Osd_location <| |>
+
+  # Update ceph.conf with OSDs present on the node
+  create_resources('osd_location', $osd_config)
 }
 
 class platform::ceph::haproxy
