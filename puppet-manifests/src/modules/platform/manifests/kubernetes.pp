3e3940824dfb830ebd39fd93265b983c6a22fc51
Dan Voiculeasa <dan.voiculeasa@windriver.com>
2021-05-13 18:03:45 +0300

    Enable kubelet support for pod pid limit
    Enable limiting the number of pids inside of pods.
    
    Add a default value to protect against a missing value.
    Default to 750 pids limit to align with service parameter default
    value for most resource consuming StarlingX optional app (openstack).
    In fact any value above service parameter minimum value is good for the
    default.
    
    Closes-Bug: 1928353
    Signed-off-by: Dan Voiculeasa <dan.voiculeasa@windriver.com>
    Change-Id: I10c1684fe3145e0a46b011f8e87f7a23557ddd4a

--- puppet-manifests/src/modules/platform/manifests/kubernetes.pp
+++ puppet-manifests/src/modules/platform/manifests/kubernetes.pp
@@ -17,6 +17,7 @@ class platform::kubernetes::params (
   $k8s_topology_mgr_policy = 'best-effort',
   $k8s_cni_bin_dir = '/usr/libexec/cni',
   $k8s_vol_plugin_dir = '/usr/libexec/kubernetes/kubelet-plugins/volume/exec/',
+  $k8s_pod_max_pids = '750',
   $join_cmd = undef,
   $oidc_issuer_url = undef,
   $oidc_client_id = undef,
@@ -135,6 +136,7 @@ class platform::kubernetes::kubeadm {
   $k8s_vol_plugin_dir = $::platform::kubernetes::params::k8s_vol_plugin_dir
   $k8s_cpu_mgr_policy = $::platform::kubernetes::params::k8s_cpu_mgr_policy
   $k8s_topology_mgr_policy = $::platform::kubernetes::params::k8s_topology_mgr_policy
+  $k8s_pod_max_pids = $::platform::kubernetes::params::k8s_pod_max_pids
 
   $iptables_file = "net.bridge.bridge-nf-call-ip6tables = 1
     net.bridge.bridge-nf-call-iptables = 1"
