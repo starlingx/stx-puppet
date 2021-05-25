b46ce5704f2b23864ea8fb1d379a53fffd656e00
Sabeel Ansari <Sabeel.Ansari@windriver.com>
2021-05-25 15:15:48 -0400

    Fix syntax error in password masking
    Fixing the syntax error in script file when
    masking passwords in the puppet logs
    
    Partial-Bug: 1896116
    
    Signed-off-by: Sabeel Ansari <Sabeel.Ansari@windriver.com>
    Change-Id: Icb20ebbd1d9c4b757af4b42b9e740ede44c0b6b2

--- puppet-manifests/src/bin/puppet-manifest-apply.sh
+++ puppet-manifests/src/bin/puppet-manifest-apply.sh
@@ -172,10 +172,10 @@ flock /var/run/puppet.lock \
     puppet apply --debug --trace --modulepath ${PUPPET_MODULES_PATH} ${PUPPET_MANIFEST} \
         < /dev/null 2>&1 | awk ' { system("date -u +%FT%T.%3N | tr \"\n\" \" \""); print $0; fflush(); } ' > ${LOGFILE}
 
-rc = $?
+rc=$?
 mask_passwd
 
-if [ rc -ne 0 ]; then
+if [ ${rc} -ne 0 ]; then
     echo "[FAILED]"
     echo "See ${LOGFILE} for details"
     exit 1
