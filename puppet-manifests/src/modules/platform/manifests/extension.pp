8390d6973e205a4af4d9ea9c8887183cb9dbb63a
Don Penney <don.penney@windriver.com>
2021-06-07 12:27:57 -0400

    Introduce runtime extension classes for patchability
    This update introduces a set of empty extension classes that can be
    used in patches to safely introduce new runtime classes. The patch can
    update an unused extension class to include the new runtime class,
    allowing sysinv to be patched to apply the selected extension class
    rather than the new runtime class directly. This allows sysinv to
    safely apply the class on a remote host, even if the host has not yet
    been patched.
    
    Closes-Bug: 1931270
    Signed-off-by: Don Penney <don.penney@windriver.com>
    Change-Id: I302d7100a52d085aa5c58f80173edb5cfd490054

---
+++ puppet-manifests/src/modules/platform/manifests/extension.pp
@@ -0,0 +1,72 @@
+#
+# Copyright (c) 2021 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Extension runtime classes improve patchability of runtime classes,
+# introducing blank classes that can be extended in a patch to
+# reference new classes. This allows sysinv to remotely call an
+# extension class in a patch safely, without concern that the new
+# runtime class introduced in a patch may not exist remotely.
+#
+
+class platform::extensions::runtime_01 {
+}
+
+class platform::extensions::runtime_02 {
+}
+
+class platform::extensions::runtime_03 {
+}
+
+class platform::extensions::runtime_04 {
+}
+
+class platform::extensions::runtime_05 {
+}
+
+class platform::extensions::runtime_06 {
+}
+
+class platform::extensions::runtime_07 {
+}
+
+class platform::extensions::runtime_08 {
+}
+
+class platform::extensions::runtime_09 {
+}
+
+class platform::extensions::runtime_10 {
+}
+
+class platform::extensions::runtime_11 {
+}
+
+class platform::extensions::runtime_12 {
+}
+
+class platform::extensions::runtime_13 {
+}
+
+class platform::extensions::runtime_14 {
+}
+
+class platform::extensions::runtime_15 {
+}
+
+class platform::extensions::runtime_16 {
+}
+
+class platform::extensions::runtime_17 {
+}
+
+class platform::extensions::runtime_18 {
+}
+
+class platform::extensions::runtime_19 {
+}
+
+class platform::extensions::runtime_20 {
+}
+
