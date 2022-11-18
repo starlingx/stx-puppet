975498969000c47f2c0bb530cf0b9fe9c7fb7189
Robert Church <robert.church@windriver.com>
2022-11-18 00:56:45 -0600

    Update root disk detection
    The root filesystem is now carved out of the cgts-vg volume group
    instead of a dedicated partition. Look for the /boot partition to
    discover the root disk.
    
    Test Plan:
    - PASS: BIOS AIO-SX
    - PASS: UEFI AIO-SX
    - PASS: BIOS 2+2+2
    - SKIP: secure boot, not ready for Stx8.0
    - PASS: AIO-SX upgrade
    - PASS: AIO-DX upgrade
    - PASS: DC subcloud install (virtual test)
    
    Change-Id: I1fd19f793f74497f3329bdadc4b7e4208b799e5d
    Depends-On: https://review.opendev.org/c/starlingx/integ/+/865076
    Story: 2010444
    Task: 46884
    Signed-off-by: Robert Church <robert.church@windriver.com>

--- puppet-manifests/src/modules/platform/lib/facter/is_primary_disk_rotational.rb
+++ puppet-manifests/src/modules/platform/lib/facter/is_primary_disk_rotational.rb
@@ -1,6 +1,6 @@
 require 'facter'
 Facter.add(:is_primary_disk_rotational) do
-  rootfs_partition = Facter::Core::Execution.exec("df --output=source / | tail -1")
+  rootfs_partition = Facter::Core::Execution.exec("df --output=source /boot | tail -1")
   rootfs_device = Facter::Core::Execution.exec("basename #{rootfs_partition} | sed 's/[0-9]*$//;s/p[0-9]*$//'")
   if rootfs_device.include? "mpath"
     rootfs_device = Facter::Core::Execution.exec("basename `readlink /dev/mapper/#{rootfs_device}`")
