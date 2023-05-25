0957d6836d0e38a292bb8d7e31ed0280c7b42dd7
Jackie Huang <jackie.huang@windriver.com>
2023-05-24 21:46:24 -0400

    dnsmasq: add ARM64 support
    The EFI file name is different for ARM64, add it
    into the manifests for configuring dnsmasq.
    
    Test Plan:
    PASS: build-pkgs on x86-64 host
    PASS: build-image on x86-64 host
    PASS: build-pkgs on arm64 host
    PASS: build-image on arm64 host
    PASS: PXE install for AIO-DX on x86-64 targets
    PASS: PXE install for AIO-DX on arm64 targets
    
    Story: 2010739
    Task: 48284
    
    Signed-off-by: Jackie Huang <jackie.huang@windriver.com>
    Change-Id: Ibb9e67988bd75e850a930f9578a9210d2aa9c6cc

--- puppet-manifests/src/modules/platform/manifests/dns.pp
+++ puppet-manifests/src/modules/platform/manifests/dns.pp
@@ -1,11 +1,23 @@
 class platform::dns::dnsmasq::params (
   $efi_bootloader = $::osfamily ? {
-    'RedHat' => 'EFI/grubx64.efi',
-    default => 'EFI/BOOT/bootx64.efi'
+    'RedHat' => $::architecture ? {
+      'aarch64' => 'EFI/grubaa64.efi',
+      'arm64' => 'EFI/grubaa64.efi',
+      default => 'EFI/grubx64.efi',
+    },
+    default => $::architecture ? {
+      'aarch64' => 'EFI/BOOT/bootaa64.efi',
+      'arm64' => 'EFI/BOOT/bootaa64.efi',
+      default => 'EFI/BOOT/bootx64.efi',
+    },
   },
   $uefi_bootloader = $::osfamily ? {
     'RedHat' => 'EFI/shim.efi',
-    default => 'EFI/BOOT/bootx64.efi'
+    default => $::architecture ? {
+      'aarch64' => 'EFI/BOOT/bootaa64.efi',
+      'arm64' => 'EFI/grubaa64.efi',
+      default => 'EFI/BOOT/bootx64.efi'
+    }
   }
 ) {}
 
