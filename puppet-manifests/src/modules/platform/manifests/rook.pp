77942b9dfc591dd156ce48be4cd2703e283b2ec9
Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>
2024-07-03 14:27:23 -0300

    Add flag file to support rook-ceph CLI
    These changes include refactoring the rook manigest to support enaling
    the ceph CLI for ceph-rook solution.
    
    The flag used for ceph CLI for ceph-rook solution is
    ".node_rook_configured" in /etc/platform
    
    Test Plan:
     PASS: Make sure the file does not exist when
           storage-backend does not exist.
     PASS: Make sure the file exists when storage-backend exists.
    
    Story: 2011117
    Task: 50499
    
    Change-Id: Ia09b8e7895bcd6afa9d30492cfd9403a597d5288
    Signed-off-by: Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>

--- puppet-manifests/src/modules/platform/manifests/rook.pp
+++ puppet-manifests/src/modules/platform/manifests/rook.pp
@@ -3,45 +3,6 @@ class platform::rook::params(
   $node_rook_configured_flag = '/etc/platform/.node_rook_configured',
 ) { }
 
-
-define platform::rook::mapping {
-  exec { 'enable volume group device mapper mapping':
-    command => "vgchange -ay ${name} || true",
-    onlyif  => "test ! -d /dev/${name}",
-  }
-}
-
-
-define platform::rook::directory(
-  $disk_node,
-  $data_path,
-  $directory,
-) {
-  exec { "mount ${disk_node}":
-    unless  => "mount | grep -q ${disk_node}",
-    command => "mount ${data_path} ${directory} || true",
-  }
-}
-
-class platform::rook::directories(
-  $dir_config = {},
-) inherits ::platform::rook::params {
-
-  create_resources('platform::rook::directory', $dir_config)
-}
-
-class platform::rook::vg::rook_vg(
-  $vg_name = [],
-) inherits platform::rook::params {
-  ::platform::rook::mapping { $vg_name:
-  }
-}
-
-class platform::rook::storage {
-    include ::platform::rook::directories
-    include ::platform::rook::vg::rook_vg
-}
-
 class platform::rook::post
   inherits ::platform::rook::params {
 
@@ -50,34 +11,30 @@ class platform::rook::post
     file { $node_rook_configured_flag:
       ensure => present
     }
+  } else {
+    # Remove the configuration file if service is not enabled
+    file { $node_rook_configured_flag:
+      ensure => absent
+    }
   }
 }
 
 class platform::rook::base
   inherits ::platform::rook::params {
 
-  $system_mode = $::platform::params::system_mode
-  $system_type = $::platform::params::system_type
-
   if $service_enabled {
     include ::platform::filesystem::ceph::mountpoint
+  }
 
-    if $system_type == 'All-in-one' and 'duplex' in $system_mode {
-      File['/var/lib/ceph'] -> Drbd::Resource <| |> -> Class[$name]
-    }
-
-    class { '::platform::rook::post':
-      stage => post
-    }
+  class { '::platform::rook::post':
+    stage => post
   }
 }
 
 class platform::rook {
-  include ::platform::rook::storage
   include ::platform::rook::base
 }
 
 class platform::rook::runtime {
-
   include ::platform::rook::base
 }
