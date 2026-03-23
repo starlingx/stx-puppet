c3504c1f04879379a67f8c225673dc50d2d70f17
Sai Lakshmi Teja Vanka <SaiLakshmiTeja.Vanka@windriver.com>
2026-03-23 04:02:37 -0400

    Update strongSwan puppet manifests for Trixie
    Debian Trixie ships strongSwan 6.0 which renames the service and
    process:
    - Service: strongswan-starter -> strongswan
    - Process: charon -> charon-systemd
    - Init command: ipsec -> strongswan
    
    Update the puppet manifests and templates to use the new names:
    - strongswan.pp: service name, pmon config path, override dir,
      exec descriptions, and restart command
    - strongswan-starter-pmond-conf.erb: process and service names
    - strongswan.systemd.override.conf.erb: add VICI socket wait
      loop for swanctl --load-all and PID file creation for pmon
    
    Also add a service resource to ensure strongswan is enabled and
    running before attempting restarts.
    
    Test Plan:
    PASS AIO-DX deploy
    PASS AIO-DX Trixie - disable/enable IPsec, pmon restart
    PASS AIO-DX Bullseye - disable/enable IPsec, pmon restart
    PASS strongswan.service enabled (Trixie), strongswan-starter.service enabled (Bullseye)
    PASS System healthy after all tests - both controllers available, zero alarms
    
    Change-Id: I62cff253ec98994d26d986bbad2a855ab19e77c3
    Signed-off-by: Sai Lakshmi Teja Vanka <SaiLakshmiTeja.Vanka@windriver.com>

--- puppet-manifests/debian/trixie/src/modules/platform/manifests/strongswan.pp
+++ puppet-manifests/debian/trixie/src/modules/platform/manifests/strongswan.pp
@@ -108,14 +108,14 @@ class platform::strongswan::swanctl_config (
 class platform::strongswan::config
   inherits ::platform::strongswan::params {
 
-  $strongswan_override_dir = '/etc/systemd/system/strongswan-starter.service.d'
-  $pmon_config_file = '/etc/pmon.d/strongswan-starter.conf'
+  $strongswan_override_dir = '/etc/systemd/system/strongswan.service.d'
+  $pmon_config_file = '/etc/pmon.d/strongswan.conf'
 
   # Restart charon
   if (find_file($pmon_config_file)) {
-    $ipsec_restart_cmd = '/usr/local/sbin/pmon-restart charon'
+    $ipsec_restart_cmd = '/usr/local/sbin/pmon-restart charon-systemd'
   } else {
-    $ipsec_restart_cmd = '/usr/bin/systemctl restart ipsec'
+    $ipsec_restart_cmd = '/usr/bin/systemctl restart strongswan'
   }
 
   # Create systemd override directory
@@ -126,7 +126,7 @@ class platform::strongswan::config
     mode   => '0755',
   }
 
-  # Add strongswan-starter service override
+  # Add strongswan service override
   -> file { "${strongswan_override_dir}/strongswan-stx-override.conf":
     content => template('platform/strongswan.systemd.override.conf.erb'),
     owner   => 'root',
@@ -135,7 +135,7 @@ class platform::strongswan::config
   }
 
   # Reload systemd
-  -> exec { 'perform systemctl daemon reload for strongswan-starter override':
+  -> exec { 'perform systemctl daemon reload for strongswan override':
     command   => '/usr/bin/systemctl daemon-reload',
     logoutput => true,
   }
@@ -173,6 +173,11 @@ class platform::strongswan::config
     is_active_controller => $platform::strongswan::params::is_active_controller,
   }
 
+  # Ensure strongswan is enabled
+  -> service { 'strongswan':
+    enable => true,
+  }
+
   # Restart charon
   -> exec { 'Restart charon to take updated configs':
     command => $ipsec_restart_cmd,
@@ -183,16 +188,16 @@ class platform::strongswan::config
   # moved to /etc/pmon.d. This is to avoid the issue where the puppet
   # generated file some times doesn't trigger an inode notification on
   # time for pmon to detect and register charon process for monitoring.
-  -> file { '/tmp/strongswan-starter.conf':
+  -> file { '/tmp/strongswan.conf':
     ensure  => file,
-    content => template('platform/strongswan-starter-pmond-conf.erb'),
+    content => template('platform/strongswan-pmond-conf.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
   }
   # Move the pmon config file to /etc/pmon.d
   -> exec { 'Move pmon config file to its location':
-    command => "/usr/bin/mv /tmp/strongswan-starter.conf ${pmon_config_file}",
+    command => "/usr/bin/mv /tmp/strongswan.conf ${pmon_config_file}",
   }
 
   # Run ipsec-cert-renew.sh daily
