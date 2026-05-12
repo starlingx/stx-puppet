761408df17edb242a8a260276f107c42a821e6ec
Caio Bruchert <caio.bruchert@windriver.com>
2026-05-12 16:58:37 -0300

    Fix ntpsec installation and upgrade for trixie
    The following items were fixed for ntpsec
    
    1. ntpsec loads /etc/default/ntpsec, but Puppet was setting the old
       /etc/default/ntp file making ntpd start with the incorrect options.
       Since this file is static, instead of creating it using Puppet, we
       now create it during ntp-config package installation.
    2. base-passwd patch was creating old ntp user and group. Now it creates
       the correct ntpsec user and group.
    3. Fix the drift file location to match Debian's default.
    4. During upgrade, ntpd could not start because the system had no ntpsec
       user and group.
       An upgrade agent-hook was created to replace the 'ntp' user and group
       with 'ntpsec' in the appropriate files.
    5. The agent hook also handles moving config files to their correct
       location.
    
    Test plan:
    PASS: check ntpd user/group after installation/upgrade/rollback
    PASS: check ntpd state after installation/upgrade/rollback
    PASS: check ntpd systemd files after installation/upgrade/rollback
    PASS: check ntpd drift file after installation/upgrade/rollback
    PASS: check ntp/ntpsec leftover files after upgrade/rollback
    
    Story: 2011744
    Task: 54485
    
    Depends-On: https://review.opendev.org/c/starlingx/config-files/+/988449
    
    Change-Id: I6d9b31b1ee67a19d8c55366a8477895e6af87248
    Signed-off-by: Caio Bruchert <caio.bruchert@windriver.com>

--- puppet-manifests/debian/trixie/src/modules/platform/manifests/ntp.pp
+++ puppet-manifests/debian/trixie/src/modules/platform/manifests/ntp.pp
@@ -108,12 +108,6 @@ class platform::ntp::server {
       mode    => '0640',
       content => template('platform/ntp_initial.conf.server.erb'),
     }
-
-    file { '/etc/default/ntp':
-      ensure  => file,
-      mode    => '0644',
-      content => 'NTPD_OPTS="-U 0"',
-    }
   }
 }
 
